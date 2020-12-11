b00abd0edaf97dac5cf918f6a9df1bdc1ef7151f
zd08 <1454647006@qq.com>
2020-12-11 10:35:07 +0800

    上传

--- assert_data/assert_result.py
+++ assert_data/assert_result.py
@@ -1,8 +1,23 @@
+from log.Log import info
 class Assert:
-    def __init__(self,move_data,result):
+    def __init__(self,move_data,result,assert_methodi):
         self.data=move_data
         self.result = result
         # print(self.data,self.result)
+        if assert_methodi == '相等':
+            self.assert_equation()
+        elif assert_methodi == '不相等':
+            self.assert_not_equation()
+        elif assert_methodi == '大于':
+            self.assert_greater()
+        elif assert_methodi == '小于':
+            self.assert_less()
+        elif assert_methodi == "in":
+            self.assert_in()
+        elif assert_methodi == "notin":
+            self.assert_notin()
+        else:
+            info("无有效断言")
 
     def assert_equation(self):
         assert self.data == self.result
